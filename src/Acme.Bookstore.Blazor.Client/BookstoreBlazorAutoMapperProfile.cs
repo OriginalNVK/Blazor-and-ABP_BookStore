19180aa39f0ac51f73d45ab9659fddd144c17a5b
OriginalNVK <nguyenvankhanh27072004@gmail.com>
2025-04-17 22:08:32 +0700

    Add Environment and Complete Part 1 to get ListBook API

---
+++ src/Acme.Bookstore.Blazor.Client/BookstoreBlazorAutoMapperProfile.cs
@@ -0,0 +1,11 @@
+using AutoMapper;
+
+namespace Acme.Bookstore.Blazor.Client;
+
+public class BookstoreBlazorAutoMapperProfile : Profile
+{
+    public BookstoreBlazorAutoMapperProfile()
+    {
+        //Define your AutoMapper configuration here for the Blazor project.
+    }
+}
